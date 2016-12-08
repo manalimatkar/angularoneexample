ed5f729f845c812ecc604052c55faae1da3d3abf
manalimatkar <manali.matkar@gmail.com>
2016-12-07 18:29:12 -0600

    angular example code push

---
+++ components/classifieds.ctr.js
@@ -0,0 +1,20 @@
+(function(){
+	"use strict";
+	angular
+		.module("ngClassifieds")
+		.controller("classifiedsCtlr", function($scope, $http, classifiedsFactory, $mdSidenav){
+			
+			classifiedsFactory.getClassifieds().then(function(classifieds){
+				$scope.classifieds = classifieds.data ;
+			});
+
+			$scope.openSidebar = function(){
+				$mdSidenav("left").open();
+			}
+			
+			$scope.closeSidebar = function(){
+				$mdSidenav("left").close();
+			}
+		});
+
+})();
